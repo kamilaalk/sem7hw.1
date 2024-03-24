2e1854b3db23455ec92ba692165e7570d7e89143
Kamila <kamila.sbz@yandex.com>
2024-03-25 00:06:28 +0500

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте произвольный массив. Выведите
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+
+void print(int [] array, int n) 
+    { 
+      if (n > 0) 
+      { 
+      	Console.Write(array[n] + " "); 
+      	print(array, n - 1); 
+      } 
+      else Console.WriteLine(array[0]); 
+    } 
+  
+      int [] array = {1, 2, 5, 0, 10, 34}; 
+      int n = 6;
+      print(array, n-1); 
+     
